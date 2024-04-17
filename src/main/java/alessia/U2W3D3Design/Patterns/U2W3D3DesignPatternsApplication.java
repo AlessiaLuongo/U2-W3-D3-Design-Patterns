a7df19d61366d3db8060d8180e71a5ad9a175f7a
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-04-17 14:46:27 +0200

    first commit

---
+++ src/main/java/alessia/U2W3D3Design/Patterns/U2W3D3DesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package alessia.U2W3D3Design.Patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W3D3DesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W3D3DesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alessia/U2W3D3Design/Patterns/U2W3D3DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package alessia.U2W3D3Design.Patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W3D3DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
